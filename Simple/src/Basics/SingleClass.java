c6e9e68fa386de8bf60fd27b5c2e60d464b65aa7
veniapputrii <veniapputrii@gmail.com>
2024-04-09 12:24:12 +0700

    version 1

---
+++ Simple/src/Basics/SingleClass.java
@@ -0,0 +1,15 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class SingleClass{
+
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int inputNumber = scanner.nextInt();
+
+        System.out.println(inputNumber);
+    }
+}
